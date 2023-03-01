40019fc3a33e845448332fa185178c0e0b850315
Oyku <oykgokcek@gmail.com>
2023-03-01 16:05:53 +0300

    başlangıç yapıldı

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,38 @@
 // ESNEK
+const defaultCars = [
+  {
+    vin: "123",
+    make: "Volkswagen",
+    model: "Golf",
+    mileage: 12345,
+  },
+  {
+    vin: "1234",
+    make: "Mercedes",
+    model: "C180",
+    mileage: 12345,
+  },
+  {
+    vin: "12345",
+    make: "Audi",
+    model: "A4",
+    mileage: 12345,
+  },
+  {
+    vin: "123456",
+    make: "Seat",
+    model: "Leon",
+    mileage: 12345,
+  },
+  {
+    vin: "1234567",
+    make: "Skoda",
+    model: "Octavia",
+    mileage: 12345,
+  },
+];
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate(); //Tüm dataları sil.
+  await knex("cars").insert(defaultCars); // Varsayılan kayıtları ekle.
+};
